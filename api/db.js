0c38c3f175a94612c896073cdde7bb8ad808a319
Sohidul-Islam <sishufol.sim@gmail.com>
2025-04-12 07:47:20 +0600

    change someting

--- api/db.js
+++ api/db.js
@@ -62,7 +62,7 @@ if (process.env.NODE_ENV === 'development') {
 // alter: true
 
 sequelize
-    .sync({ force: true })
+    .sync({ alter: true })
     .then(async (res) => {
         console.log("Database Connected", process.env.PORT);
     })
